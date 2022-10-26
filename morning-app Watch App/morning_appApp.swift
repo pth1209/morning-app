719792fd7e2e656ea833f7600481152802420dce
pth1209 <lawrencepark1@gmail.com>
2022-10-25 20:28:38 -0500

    Initial Commit

---
+++ morning-app Watch App/morning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  morning_appApp.swift
+//  morning-app Watch App
+//
+//  Created by Lawrence Park on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct morning_app_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ morning-app/morning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  morning_appApp.swift
+//  morning-app
+//
+//  Created by Lawrence Park on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct morning_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
